3658bd00059c797e90fb397d1b9e4244bb243bb8
Nurbek878 <nurik878878@yandex.ru>
2023-09-02 01:00:47 +0500

    Add custom pagination

--- backend/food/settings.py
+++ backend/food/settings.py
@@ -84,8 +84,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 6,
+    'DEFAULT_PAGINATION_CLASS': 'api.core.pagination.FoodSetPagination',
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
