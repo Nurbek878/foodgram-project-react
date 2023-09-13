e5e6f016fc1b544ae3cb55350a73165aa913ea6c
Nurbek878 <nurik878878@yandex.ru>
2023-09-14 00:56:14 +0500

    Edit and deploy

--- backend/food/settings.py
+++ backend/food/settings.py
@@ -1,14 +1,17 @@
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'default')
 
 DEBUG = os.getenv('DEBUG', default=True)
 
-ALLOWED_HOSTS = ['62.84.123.183', 'foodproject.hopto.org',
-                 '127.0.0.1', 'localhost', 'backend']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(', ')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
